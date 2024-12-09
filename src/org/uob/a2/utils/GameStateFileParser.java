ebe3f33cb411ece0a124a16bd68f713c65854368
arty <axj450@student.bham.ac.uk>
2024-12-09 04:17:31 +0000

    Commit 1

--- src/org/uob/a2/utils/GameStateFileParser.java
+++ src/org/uob/a2/utils/GameStateFileParser.java
@@ -32,56 +32,59 @@ public class GameStateFileParser {
 
         try {
             File textFile = new File(filename);
+
+
+            Scanner fileInput = new Scanner(textFile);
+            while (fileInput.hasNextLine()) {
+                int counter = 0;
+                String line = fileInput.nextLine();
+                String[] lineSplit = line.split(":");
+                String[] objectParts = lineSplit[1].split(",");
+                for (int i = 0; i < objectParts.length; i++) {
+                    if (objectParts[i].equalsIgnoreCase("true") || objectParts[i].equalsIgnoreCase("false")) {
+                        hidden = Boolean.parseBoolean(objectParts[i]);
+                    }
+                }
+                switch (lineSplit[0]) {
+                    case "player":
+                        player = new Player(lineSplit[1]);
+                        break;
+                    case "room":
+                        roomArrayList.add(new Room(objectParts[0], objectParts[1], objectParts[2], hidden));
+                        break;
+                    case "equipment":
+                        useInformationArrayList.add(new UseInformation(false, objectParts[4], objectParts[5], objectParts[6], objectParts[7]));
+                        equipmentArrayList.add(new Equipment(objectParts[0], objectParts[1], objectParts[2], hidden, useInformationArrayList.get(counter)));
+                        equipmentList.addEquipment(equipmentArrayList.get(counter));
+                        break;
+                    case "container":
+                        containerArrayList.add(new Container(objectParts[0], objectParts[1], objectParts[2], hidden));
+                        break;
+                    case "exit":
+                        exitArrayList.add(new Exit(objectParts[0], objectParts[1], objectParts[2], objectParts[3], hidden));
+                        break;
+                    case "feature":
+                        featureArrayList.add(new Feature(objectParts[0], objectParts[1], objectParts[2], hidden));
+                        break;
+                    case "item":
+                        itemArrayList.add(new Item(objectParts[0], objectParts[1], objectParts[2], hidden));
+                        itemList.addItem(itemArrayList.get(counter));
+                        break;
+
+                }
+                counter++;
+
+            }
+            Map map = new Map();
+            GameState gameState = new GameState(map, player);
+            map.setCurrentRoom("r1");
+
+            return gameState;
         }
-        catch(FileNotFoundException){
+
+        catch(Exception e){
             System.out.println("File not found");
         }
-        Scanner fileInput = new Scanner(textFile);
-        while (fileInput.hasNextLine()) {
-            int counter = 0;
-            String line = fileInput.nextLine();
-            String[] lineSplit = line.split(":");
-            String[] objectParts = lineSplit[1].split(",");
-            for (int i = 0; i < objectParts.length; i++) {
-                if (objectParts[i].equalsIgnoreCase("true") || objectParts[i].equalsIgnoreCase("false")) {
-                    hidden = Boolean.parseBoolean(objectParts[i]);
-                }
-            }
-            switch (lineSplit[0]) {
-                case "player":
-                    player = new Player(lineSplit[1]);
-                    break;
-                case "room":
-                    roomArrayList.add(new Room(objectParts[0], objectParts[1], objectParts[2], hidden));
-                    break;
-                case "equipment":
-                    useInformationArrayList.add(new UseInformation(false, objectParts[4], objectParts[5], objectParts[6], objectParts[7]));
-                    equipmentArrayList.add(new Equipment(objectParts[0], objectParts[1], objectParts[2], hidden, useInformationArrayList.get(counter)));
-                    equipmentList.addEquipment(equipmentArrayList.get(counter));
-                    break;
-                case "container":
-                    containerArrayList.add(new Container(objectParts[0], objectParts[1], objectParts[2], hidden));
-                    break;
-                case "exit":
-                    exitArrayList.add(new Exit(objectParts[0], objectParts[1], objectParts[2], objectParts[3], hidden));
-                    break;
-                case "feature":
-                    featureArrayList.add(new Feature(objectParts[0], objectParts[1], objectParts[2], hidden));
-                    break;
-                case "item":
-                    itemArrayList.add(new Item(objectParts[0], objectParts[1], objectParts[2], hidden));
-                    itemList.addItem(itemArrayList.get(counter));
-                    break;
-
-            }
-            counter++;
-
-        }
-        Map map = new Map();
-        GameState gameState = new GameState(map, player);
-        map.setCurrentRoom("r1");
-
-        return gameState;
 
     }
 }
