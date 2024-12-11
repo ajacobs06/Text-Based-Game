98027971bcc756e50c8cef6b40e8ebcd2969c36d
arty <axj450@student.bham.ac.uk>
2024-12-11 04:13:05 +0000

    Commit 1

--- src/org/uob/a2/commands/Status.java
+++ src/org/uob/a2/commands/Status.java
@@ -27,46 +27,43 @@ public class Status extends Command {
 
     @Override
     public String execute(GameState gameState) {
-        if(topic.equals("inventory")) {
-            ArrayList<Equipment> equipment = gameState.getPlayer().getEquipment();
-            ArrayList<Item> items = gameState.getPlayer().getInventory();
-            ArrayList<String> inventory = new ArrayList<String>();
-            if(items.size()>0) {
-                for (Item i : items) {
-                    inventory.add(i.getName() + ",");
+        try {
+            if (topic.equals("inventory")) {
+                ArrayList<Equipment> equipment = gameState.getPlayer().getEquipment();
+                ArrayList<Item> items = gameState.getPlayer().getInventory();
+                ArrayList<String> inventory = new ArrayList<String>();
+                if (items.size() > 0) {
+                    for (Item i : items) {
+                        inventory.add(i.getName() + ",");
+                    }
                 }
-            }
-            if(equipment.size()>0) {
-                for (Equipment i : equipment) {
-                    inventory.add(i.getName() + ",");
+                if (equipment.size() > 0) {
+                    for (Equipment i : equipment) {
+                        inventory.add(i.getName() + ",");
+                    }
                 }
+                if (inventory.equals("")) {
+                    return "Inventory: There are no items in your inventory";
+                } else {
+                    return "Inventory: " + inventory.toString();
+                }
+            } else if (topic.equals("player")) {
+                return gameState.getPlayer().toString();
+            } else if (topic.equals("map")) {
+                return gameState.getMap().display();
+            } else if (gameState.getPlayer().hasEquipment(topic)) {
+                return gameState.getPlayer().getEquipment(topic).getDescription();
+            } else if (gameState.getPlayer().hasItem(topic)) {
+                return gameState.getPlayer().getItem(topic).getDescription();
+            } else if (gameState.getMap().getCurrentRoom().hasEquipment(topic)) {
+                return gameState.getMap().getCurrentRoom().getEquipment(topic).getDescription();
+            } else if (gameState.getMap().getCurrentRoom().hasItem(topic)) {
+                return gameState.getMap().getCurrentRoom().getItem(topic).getDescription();
+            }  else {
+                return "";
             }
-            if(inventory.equals("")) {
-                return "Inventory: There are no items in your inventory";
-            }
-            else {
-                return "Inventory: " + inventory.toString();
-            }
         }
-        else if(gameState.getPlayer().hasEquipment(topic)) {
-            return gameState.getPlayer().getEquipment(topic).getDescription();
-        }
-        else if(gameState.getPlayer().hasItem(topic)) {
-            return gameState.getPlayer().getItem(topic).getDescription();
-        }
-        else if(gameState.getMap().getCurrentRoom().hasEquipment(topic)) {
-            return gameState.getMap().getCurrentRoom().getEquipment(topic).getDescription();
-        }
-        else if(gameState.getMap().getCurrentRoom().hasItem(topic)) {
-            return gameState.getMap().getCurrentRoom().getItem(topic).getDescription();
-        }
-        else if(topic.equals("player")){
-            return gameState.getPlayer().toString();
-        }
-        else if(topic.equals("map")){
-            return gameState.getMap().display();
-        }
-        else{
+        catch(NullPointerException e) {
             return "";
         }
     }
